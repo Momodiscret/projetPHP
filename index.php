0959418bc95ee68f83b1ff79d760e0e99831cb10
Momodiscret <ibrahim.mohamed@uit.ac.ma>
2024-02-25 14:38:40 +0100

    Ajout des fichiers

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: lightblue;
+                
+        }
+    </style>
+</head>
+<body>
+    
+    <form action="reup.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Name: <input type="text" name="name">
+        E-mail: <input type="text" name="email">
+        Website: <input type="text" name="website">
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+        Gender:
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other
+        <input type="submit">
+    </form>
+
+</body>
+</html>
